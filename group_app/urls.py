2c580c2bdec8204c5670b611087856956ba8be7c
Riley Evans <mysamari@gmail.com>
2021-04-22 21:26:04 -0600

    profile.html/css and  views

--- group_app/urls.py
+++ group_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('profile',views.profile),
-    
+    path('contact',views.contact)
 ]
--- group_app/views.py
+++ group_app/views.py
@@ -6,7 +6,8 @@ def index(request):
 
     return render(request,'index.html')
 
-    
+def contact (request):
+    return render(request,'contact.html')
 
 
 def profile(request):
