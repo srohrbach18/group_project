937f3e94b2c59724a5160d94a48192d462189d95
David Johnson <johnsodl84@gmail.com>
2021-04-24 11:50:42 -0700

    welcome.html update

--- group_app/urls.py
+++ group_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('register', views.register),
+    path('login', views.login),
+    path('welcome', views.welcome),
+    
 ]
