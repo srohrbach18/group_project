bc816616cfaf475770a320af60137fb93938ce64
MR ME <mysamari@gmail.com>
2021-04-25 14:35:35 -0600

    removed max_num in int fields for models

---
+++ group_project/group_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Item(models.Model):
+    desc = models.TextField(max_length=100)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Menu (models.Model):
+    item = models.ManyToManyField(Item)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Pay (models.Model):
+    # user = models.ForeignKey(User,related_name='pay' ,on_delete=models.CASCADE)
+    # card_num = models.ForeignKey(User,on_delete=models.Cascade)
+    card = models.IntegerField()
+    exp = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Billing (models.Model):
+    address = models.TextField(max_length=100)
+    phone_number = models.IntegerField()
+    state = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
